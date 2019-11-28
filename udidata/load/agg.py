87aa600d33aa80d29b725c1e7c48ad31d651ebb5
Udi Yosovzon <udiyosovzon@gmail.com>
2019-11-28 22:01:20 +0200

    improve

--- udidata/load/agg.py
+++ udidata/load/agg.py
@@ -1,39 +1,28 @@
-from ..dir.utils import DATA_DIR, add_lead_zero
+from ..dir.utils import DATA_DIR, add_lead_zero, get_day_folder_path
 import pandas as pd
 import xarray as xr
 
 #######################################################################################################################
 
-def day(date, column=None, latlng=None):
+def day(date):
+    
     """
-    Returns a pandas DataFrame of daily aggregated data
-
-    Parameters
-    ----------
-    date : str 
-        Expected date format is yyyy/mm/dd
-
-    column : str, default None
-        Specify a spcecific column if desried
-
-    latlng : tuple of floats, default None
-        Specify specific latitude and longitude if desried
-        Expected format is (lat,lng)
-
-    Returns
-    -------
-    df : an aggregated pandas Dataframe
+    Load daily agg data for a specifc date
     """
+    path_start = f"{get_day_folder_path(date)}{date.replace('/','')}_"    
+    df = pd.read_csv(f"{path_start}daily_agg.csv.gz", index_col=["lat", "lng", "stat"])
+    return df
 
-    df = pd.read_csv(f"{DATA_DIR}/{date}/{date.replace('/','')}_daily_agg.csv.gz", index_col=[0,1], header=[0,1]) 
-    df.index.names = ["lat", "lng"]
-    df.columns.names = ["atmos", "stat"]
-                     
-    if column:
-        df = df[["count", column]].droplevel(0, axis=1)
-    if latlng:
-        df = df.loc[[latlng]]
-    return df 
+#######################################################################################################################
+
+def hourly(date):
+    
+    """
+    Load hourly agg data for a specifc date
+    """
+    path_start = f"{get_day_folder_path(date)}{date.replace('/','')}_"    
+    df = pd.read_csv(f"{path_start}hourly_agg.csv.gz", index_col=["hour", "lat", "lng", "stat"])
+    return df
 
 #######################################################################################################################
 
