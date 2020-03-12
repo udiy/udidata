2fb89c4bdda43788e13d1a32f45bd09d327cb517
Udi <udiyosovzon@gmail.com>
2020-03-12 18:37:38 +0200

    improve fft

--- udidata/calculate/fft.py
+++ udidata/calculate/fft.py
@@ -4,29 +4,48 @@ import pandas as pd
 
 #######################################################################################################################
 
-def fft(signal, sampling_rate):
+def fft(signal, sampling_rate, positive=False, spectrum=None):
     
     """
     Parameters
     ----------
     signal: array-like
-        An array with discrete signal values
+        An array with discrete signal values.
     
     sampling_rate: int
         Number of samples per one cycle of a unit time.
 
+    positive: bool
+        If true, return only positive frequency values
+
+    spectrum: str
+        Type of spectral values to return. Options: "amp" - amplitude, "psd" - power spectral density (amplitude sqaured).
+        If None than complex numbers will be returned
+
     Returns
     -------
-    S, freq: tuple
-        Tuple of two arrays one for intensity (PSD), one for frequency domain
+    S: pandas.Series
+        A pandas series whose values is the frequecny spectrum, and index is freqeuncy values
     """
-    Ts = 1/sampling_rate    # sampling spacing - inverse of sampling rate
-    N = len(signal)    # number of sampled points
-    
-    S = np.fft.rfft(signal)
-    S = np.abs(S)/N    # normalize frequency
-    freq = np.fft.rfftfreq(N, Ts)    # frequecny domain
 
-    return pd.Series(S, index=pd.Index(freq, name="freq"))
+    # compute frequency spectrum using fft
+    S = np.fft.fft(signal)
     
+    # compute frequency range (frequency axis values)
+    n = signal.size
+    d = (1/sampling_rate)   # sampling spacing - inverse of sampling rate
+    freq = np.fft.fftfreq(n, d)
+
+    # cast S into a pandas Series with frequency values as index
+    S = pd.Series(S, index=pd.Index(freq, name="freq"))
+
+    if spectrum == "amp":
+        S = S.abs()
+    elif spectrum == "psd":
+        S = (S.abs() ** 2)
     
+    if positive:
+        # slice S to select only positive frequency values
+        return S.iloc[:int(n/2)]
+
+    return S
